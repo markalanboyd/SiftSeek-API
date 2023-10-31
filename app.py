c0ee56d717a6df7787e3542e501f6e781b0618a2
Mark Boyd <10804241+markalanboyd@users.noreply.github.com>
2023-10-31 06:41:59 +0100

    Update app.py
    I'm switching over to using Flask on the advice of my mentor, as it's better documented than Litestar and is a more widely used framework.

--- app.py
+++ app.py
@@ -1,49 +1,12 @@
-from dataclasses import dataclass
+from flask import Flask, render_template
 
-from litestar import Litestar, get, post, put
-from litestar.exceptions import NotFoundException
-
-
-@dataclass
-class TodoItem:
-    title: str
-    done: bool
+app = Flask(__name__)
 
 
-TODO_LIST: list[TodoItem] = [
-    TodoItem(title="Start writing TODO list", done=True),
-    TodoItem(title="???", done=False),
-    TodoItem(title="Profit", done=False),
-]
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
 
 
-def get_todo_by_title(todo_name) -> TodoItem:
-    for item in TODO_LIST:
-        if item.title == todo_name:
-            return item
-    raise NotFoundException(detail=f"TODO {todo_name!r} not found")
-
-
-@get("/")
-async def get_list(done: bool | None = None) -> list[TodoItem]:
-    if done is None:
-        return TODO_LIST
-    return [item for item in TODO_LIST if item.done == done]
-
-
-@post("/")
-async def add_item(data: TodoItem) -> list[TodoItem]:
-    TODO_LIST.append(data)
-
-    return TODO_LIST
-
-
-@put("/{item_title:str}")
-async def update_item(item_title: str, data: TodoItem) -> list[TodoItem]:
-    todo_item = get_todo_by_title(item_title)
-    todo_item.title = data.title
-    todo_item.done = data.done
-    return TODO_LIST
-
-
-app = Litestar([get_list, add_item, update_item])
+if __name__ == "__main__":
+    app.run(debug=True)
