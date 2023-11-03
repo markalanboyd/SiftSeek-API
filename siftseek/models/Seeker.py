ac1041731e31af0233628842c459b864b7cb25d2
Mark Boyd <10804241+markalanboyd@users.noreply.github.com>
2023-11-03 07:36:42 +0100

    Update Seeker.py
    Working on the schema for the Seeker class. I'm realizing I should probably just go through LinkedIn and copy most of the info that they look for from job seekers rather than trying to just think of everything on my own.

--- siftseek/models/Seeker.py
+++ siftseek/models/Seeker.py
@@ -1,14 +1,48 @@
 from .db import db
-from sqlalchemy import Integer, DateTime
-from sqlalchemy.orm import Mapped, mapped_column as mc
+from sqlalchemy import String, Integer, DateTime, Boolean
+from sqlalchemy.orm import Mapped, mapped_column
 from datetime import datetime
 
 
 class Seeker(db.Model):
-    id: Mapped[int] = mc(Integer, primary_key=True)
-    created_at: Mapped[DateTime] = mc(
+    __tablename__ = "seekers"
+    DEFAULT_PROFILE_PIC_URL: str = "https://as1.ftcdn.net/v2/jpg/05/16/27/58/1000_F_516275801_f3Fsp17x6HQK0xQgDQEELoTuERO4SsWV.jpg"
+
+    # Metadata
+    id: Mapped[Integer] = mapped_column(Integer, primary_key=True)
+    _created_at: Mapped[DateTime] = mapped_column(
         DateTime, default=datetime.utc.now, nullable=False
     )
-    modified_at: Mapped[DateTime] = mc(DateTime, nullable=False)
-    deleted_at: Mapped[DateTime] = mc(DateTime)
-    personal_details_id: Mapped[int] = mc()
+    modified_at: Mapped[DateTime] = mapped_column(DateTime, nullable=False)
+    deleted_at: Mapped[DateTime] = mapped_column(DateTime)
+
+    # Personal Info
+    username: Mapped[String] = mapped_column(String(15), unique=True)
+    first_name: Mapped[String] = mapped_column(String(255), nullable=False)
+    last_name: Mapped[String] = mapped_column(String(255), nullable=False)
+    contact_email: Mapped[String] = mapped_column(
+        String(255), nullable=False, unique=True
+    )
+    work_phone: Mapped[String] = mapped_column(String(255))
+    work_phone_ext: Mapped[String] = mapped_column(String(255))
+    cell_phone: Mapped[String] = mapped_column(String(255))
+    address: Mapped[String] = mapped_column(String(255))
+    city: Mapped[String] = mapped_column(String(255))
+
+    # URLs
+    resume_url: Mapped[String] = mapped_column(
+        String(2000), default=DEFAULT_PROFILE_PIC_URL, unique=True
+    )
+    resume_url: Mapped[String] = mapped_column(String(2000), unique=True)
+    linkedin_url: Mapped[String] = mapped_column(String(2000), unique=True)
+    portfolio_url: Mapped[String] = mapped_column(String(2000), unique=True)
+
+    # Resume Info
+    summary: Mapped[String] = mapped_column(String(10_000))
+    education_level_id: Mapped[Integer] = mapped_column(Integer)
+    education_years: Mapped[Integer] = mapped_column(Integer(2))
+    remote_option: Mapped[Boolean] = mapped_column()
+
+    @property
+    def created_at(self) -> datetime:
+        return self._created_at
