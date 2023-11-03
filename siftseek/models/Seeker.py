6348108dd63f34ca21a1287d682d379567ba8218
Mark Boyd <10804241+markalanboyd@users.noreply.github.com>
2023-11-03 21:59:14 +0100

    Profile POST method now works
    Although these certainly aren't the final values for the profile, I have it up and working. Next is to write a GET request that will return a profile.

--- siftseek/models/Seeker.py
+++ siftseek/models/Seeker.py
@@ -1,7 +1,8 @@
+from datetime import datetime
+
 from .db import db
 from sqlalchemy import String, Integer, DateTime, Boolean
 from sqlalchemy.orm import Mapped, mapped_column
-from datetime import datetime
 
 
 class Seeker(db.Model):
@@ -10,37 +11,39 @@ class Seeker(db.Model):
 
     # Metadata
     id: Mapped[Integer] = mapped_column(Integer, primary_key=True)
-    _created_at: Mapped[DateTime] = mapped_column(
-        DateTime, default=datetime.utc.now, nullable=False
-    )
-    modified_at: Mapped[DateTime] = mapped_column(DateTime, nullable=False)
-    deleted_at: Mapped[DateTime] = mapped_column(DateTime)
+    _created_at: Mapped[DateTime] = mapped_column(DateTime, default=datetime.utcnow())
+    modified_at: Mapped[DateTime] = mapped_column(DateTime)
+    deleted_at: Mapped[DateTime] = mapped_column(DateTime, nullable=True)
 
     # Personal Info
     username: Mapped[String] = mapped_column(String(15), unique=True)
-    first_name: Mapped[String] = mapped_column(String(255), nullable=False)
-    last_name: Mapped[String] = mapped_column(String(255), nullable=False)
-    contact_email: Mapped[String] = mapped_column(
-        String(255), nullable=False, unique=True
-    )
-    work_phone: Mapped[String] = mapped_column(String(255))
-    work_phone_ext: Mapped[String] = mapped_column(String(255))
-    cell_phone: Mapped[String] = mapped_column(String(255))
-    address: Mapped[String] = mapped_column(String(255))
-    city: Mapped[String] = mapped_column(String(255))
+    first_name: Mapped[String] = mapped_column(String(255))
+    last_name: Mapped[String] = mapped_column(String(255))
+    contact_email: Mapped[String] = mapped_column(String(255), unique=True)
+    work_phone: Mapped[String] = mapped_column(String(255), nullable=True)
+    work_phone_ext: Mapped[String] = mapped_column(String(255), nullable=True)
+    cellphone: Mapped[String] = mapped_column(String(255), nullable=True)
+    address: Mapped[String] = mapped_column(String(255), nullable=True)
+    city: Mapped[String] = mapped_column(String(255), nullable=True)
 
     # URLs
-    resume_url: Mapped[String] = mapped_column(
-        String(2000), default=DEFAULT_PROFILE_PIC_URL, unique=True
+    profile_pic_url: Mapped[String] = mapped_column(
+        String(2000), default=DEFAULT_PROFILE_PIC_URL
     )
-    resume_url: Mapped[String] = mapped_column(String(2000), unique=True)
-    linkedin_url: Mapped[String] = mapped_column(String(2000), unique=True)
-    portfolio_url: Mapped[String] = mapped_column(String(2000), unique=True)
+    resume_url: Mapped[String] = mapped_column(String(2000), nullable=True, unique=True)
+    linkedin_url: Mapped[String] = mapped_column(
+        String(2000), nullable=True, unique=True
+    )
+    portfolio_url: Mapped[String] = mapped_column(
+        String(2000), nullable=True, unique=True
+    )
 
     # Resume Info
-    summary: Mapped[String] = mapped_column(String(10_000))
-    education_level_id: Mapped[Integer] = mapped_column(Integer)
-    remote_option: Mapped[Boolean] = mapped_column(Boolean, default=False)
+    summary: Mapped[String] = mapped_column(String(10_000), nullable=True)
+    education_level_id: Mapped[Integer] = mapped_column(Integer, nullable=True)
+    remote_option: Mapped[Boolean] = mapped_column(
+        Boolean, default=False, nullable=True
+    )
 
     @property
     def created_at(self) -> datetime:
--- siftseek/routes/seeker/seeker_views.py
+++ siftseek/routes/seeker/seeker_views.py
@@ -1,9 +1,42 @@
+from datetime import datetime
+
+from flask import request
+
+from siftseek.models.db import db
+from siftseek.models.seeker import Seeker
 from . import seeker
 
 
 @seeker.post("/profile")
 def post_profile():
-    return "Here we'll post the profile"
+    data = request.get_json()
+    new_seeker = Seeker(
+        # Metadata
+        modified_at=datetime.utcnow(),
+        # Personal Info
+        username=data.get("username"),
+        first_name=data.get("first_name"),
+        last_name=data.get("last_name"),
+        contact_email=data.get("contact_email"),
+        work_phone=data.get("work_phone"),
+        work_phone_ext=data.get("work_phone_ext"),
+        cellphone=data.get("cellphone"),
+        address=data.get("address"),
+        city=data.get("city"),
+        # URLs
+        resume_url=data.get("resume_url"),
+        linkedin_url=data.get("linkedin_url"),
+        portfolio_url=data.get("portfolio_url"),
+        # Resume Info
+        summary=data.get("summary"),
+        education_level_id=data.get("education_level_id"),
+        remote_option=data.get("remote_option"),
+    )
+
+    db.session.add(new_seeker)
+    db.session.commit()
+
+    return "Added to DB"
 
 
 @seeker.get("/profile")
