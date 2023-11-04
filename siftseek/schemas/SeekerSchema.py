fb36bc6d209494f90ed289ebe821c96d334d67c9
Mark Boyd <10804241+markalanboyd@users.noreply.github.com>
2023-11-04 19:09:54 +0100

    Add: Schema Validators
    - Added validators for the schema.
    - Tried to implement DRY and use variables to store the validators make it more readable.
    - I broke the code somewhere, will fix it tomorrow

--- siftseek/schemas/SeekerSchema.py
+++ siftseek/schemas/SeekerSchema.py
@@ -1,14 +1,44 @@
-from marshmallow import fields, validate, post_dump
-from marshmallow.validate import Length, Email
+from marshmallow import fields, post_dump
+from marshmallow.validate import Length, Email, URL, Range
 
 from siftseek.extensions import ma
 from siftseek.models.seeker import Seeker
-from siftseek.schemas.utils import nest_data
+from siftseek.schemas.utils import nest_data, validate_phone_number
 
 
 class SeekerSchema(ma.SQLAlchemyAutoSchema):
-    username = fields.String(validate=Length(min=1, max=20))
-    contact_email = fields.Email(validate=Email())
+    # Validators
+    length_error = "Length must be between {min} and {max} characters."
+    length_255_validator = fields.String(
+        validate=Length(min=1, max=255, error=length_error)
+    )
+    email_validator = fields.Email(validate=Email(error="Invalid email address."))
+    phone_number_validator = fields.String(validate=validate_phone_number())
+    phone_extension_validator = fields.Integer(
+        validate=Length(min=1, max=5), error=length_error
+    )
+    url_validator = fields.Url(validate=URL(error="Invalid URL."))
+    id_validator = fields.Integer(
+        validate=Range(min=1, error="Value must be 1 or greater.")
+    )
+    boolean_validator = fields.Boolean(error="Invalid input for boolean field.")
+
+    username = fields.String(validate=Length(min=1, max=20, error=length_error))
+    first_name = length_255_validator
+    last_name = length_255_validator
+    contact_email = email_validator
+    work_phone = phone_number_validator
+    work_phone_ext = phone_extension_validator
+    cellphone = phone_number_validator
+    address = length_255_validator
+    city = length_255_validator
+    profile_pic_url = url_validator
+    resume_url = url_validator
+    linkedin_url = url_validator
+    portfolio_url = url_validator
+    summary = fields.String(validate=Length(min=1, max=10_000, error=length_error))
+    education_level_id = id_validator
+    remote_option = boolean_validator
 
     class Meta:
         model = Seeker
@@ -16,6 +46,7 @@ class SeekerSchema(ma.SQLAlchemyAutoSchema):
         include_fk = True
         include_relationships = True
 
+    # Define schema for categories of fields
     metadata_fields = {
         "id",
         "_created_at",
--- siftseek/schemas/utils.py
+++ siftseek/schemas/utils.py
@@ -1,14 +1,15 @@
-from typing import Dict, Any, Set
+from typing import Any, Set
 
 from marshmallow import post_dump
+from marshmallow.validate import Regexp
 
 
-def partition_data_by_fields(data: Dict[str, Any], fields: Set[str]) -> dict:
+def partition_data_by_fields(data: dict[str, Any], fields: Set[str]) -> dict:
     """
     Segregates the specified fields from the data into a separate dictionary.
 
     Args:
-        data (Dict[str, Any]): The original dictionary containing all fields.
+        data (dict[str, Any]): The original dictionary containing all fields.
         fields (Set[str]): A set of field names to partition from the original data.
 
     Returns:
@@ -17,18 +18,25 @@ def partition_data_by_fields(data: Dict[str, Any], fields: Set[str]) -> dict:
     return {key: data.pop(key) for key in list(fields) if key in data}
 
 
-def nest_data(data: Dict[str, Any], field_categories_dict: Dict[str, Set]) -> dict:
+def nest_data(data: dict[str, Any], field_categories_dict: dict[str, set]) -> dict:
     """
     Nests each field category and its fields into a dictionary.
 
     Args:
-        data (Dict[str, Any]): The original dictionary containing all fields.
-        field_categories (Dict[str, Set]): A dictionary of sets of field names to partition from the original data.
+        data (dict[str, Any]): The original dictionary containing all fields.
+        field_categories (dict[str, set]): A dictionary of sets of field names to partition from the original data.
 
     Returns:
-        dict: _description_
+        dict: A dataset nested by categories.
     """
     nested_data = {}
     for category, fields in field_categories_dict.items():
         nested_data[category] = partition_data_by_fields(data, fields)
     return nested_data
+
+
+def validate_phone_number():
+    return Regexp(
+        r"^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$",
+        error="Invalid phone number format.",
+    )
